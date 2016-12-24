9bd4543e63907e06c11f719f2da1f713d32cc7e8
Neel Smith <dnsmith.neel@gmail.com>
2016-12-24 06:53:25 -0500

    Test more properties of token

--- src/test/scala/TestHmtToken.scala
+++ src/test/scala/TestHmtToken.scala
@@ -11,8 +11,13 @@ class HmtTokenSpec extends FlatSpec {
     assert (tkn.readings(0).status == Clear)
     assert (tkn.readings(0).reading == "μῆνιν")
   }
-  it should "have a lexical type" in pending
-  it should "have a lang value" in pending
+  it should "have a lexical type" in {
+    assert (tkn.lexicalCategory == LexicalToken)
+  }
+  it should "have a default lang value of 'grc'" in {
+    assert (tkn.lang == "grc")
+  }
+  it should "have support explicitly set lang value" in pending
   it should "have a URN" in {
     assert (tkn.urn == "urn:cts:greekLit:tlg0012.tlg001.msA.urtoken:1.1.1")
   }
