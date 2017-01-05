77593b5bac39f1babdb39acac106e711e2a36c7e
Neel Smith <dnsmith.neel@gmail.com>
2017-01-05 16:07:15 -0500

    Place holder for token text deformation

--- src/main/scala/org/homermultitext/edmodel/TextDeformation.scala
+++ src/main/scala/org/homermultitext/edmodel/TextDeformation.scala
@@ -18,9 +18,26 @@ object TextDeformation {
   }
 
 
+  // could expand this fuller Leiden represnetaiotn for alt readings...
+  def readFullTokenAnalysis(tkn: HmtToken):  TextDeformation = {
+    var buff = StringBuilder.newBuilder
+    for (r <- tkn.readings) {
+      r.status match {
+        case Restored => buff.append("[" + r.reading + "]")
+        case Unclear => {
+          buff.append(r.reading.split(".").mkString("?") + "?")
+        }
+        case _ => buff.append(r.reading)
+      }
+    }
+    TextDeformation(buff.toString)
+  }
+
+
   def apply(hmtToken : HmtToken, rdg: String): TextDeformation = {
     rdg match {
       case "dipl" => readDiplomatic(hmtToken)
+      case "token" => readFullTokenAnalysis(hmtToken)
       case _ => throw new Exception("TextDeformation: unrecognized or unimplemented reading key " + rdg)
     }
   }
