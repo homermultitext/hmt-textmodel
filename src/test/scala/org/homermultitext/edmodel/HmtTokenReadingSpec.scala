26cb3881091775ec3832425f96d63360bff0ac24
Neel Smith <dnsmith.neel@gmail.com>
2017-07-03 09:12:14 -0400

    Add tests on token reading'

---
+++ src/test/scala/org/homermultitext/edmodel/HmtTokenReadingSpec.scala
@@ -0,0 +1,16 @@
+package org.homermultitext.edmodel
+import org.scalatest.FlatSpec
+import edu.holycross.shot.cite._
+
+import scala.collection.mutable.ArrayBuffer
+
+class HmtTokenReadingSpec extends FlatSpec {
+
+
+
+
+  "A token analysis"  should "have an identifying URN" in  pending
+
+
+
+}
