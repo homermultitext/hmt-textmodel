83c1b26e96add1c432f6956e0d4f227eb8ed19fd
Neel Smith <dnsmith.neel@gmail.com>
2019-01-17 13:14:41 -0500

    Expand edition types

--- src/main/scala/org/homermultitext/edmodel/EditionTypes.scala
+++ src/main/scala/org/homermultitext/edmodel/EditionTypes.scala
@@ -2,10 +2,32 @@ package org.homermultitext.edmodel
 import edu.holycross.shot.mid.validator._
 
 
-// Implementations of the [[MidEditionType]] trait.
+// Implementations of the [[MidEditionType]] trait for Homer Mulitext project editions.
+
+
 /**  */
 case object HmtNamedEntityEdition extends MidEditionType {
   def label =  "named entities"
   def description = "Tokenization all tokens to Option[Cite2Urn] of named entities"
-  def versionId = "ne"
+  def versionId = "hmt_ne"
+}
+
+
+case object HmtDiplomaticEdition extends MidEditionType {
+  def label =  "diplomatic edition"
+  def description = "Strict diplomatic version of original text"
+  def versionId = "hmt_dipl"
+}
+
+
+case object HmtScribalNormalizedEdition extends MidEditionType {
+  def label =  "scribal edition"
+  def description = "Diplomatic edition including scribal corrections and normalizations"
+  def versionId = "hmt_scribal"
+}
+
+case object HmtEditorsNormalizedEdition extends MidEditionType {
+  def label =  "editorially normalized edition"
+  def description = "Edition normalized to modern orthographic standards."
+  def versionId = "hmt_normalized"
 }
